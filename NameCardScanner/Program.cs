498c8747fe52863d6f5547dec518de0ecef5b457
mju <mju@cimpress.com>
2017-10-02 18:48:28 +0900

    CORS

--- NameCardScanner/Program.cs
+++ NameCardScanner/Program.cs
@@ -8,7 +8,7 @@ namespace NamecardScanner
     {
         static void Main(string[] args)
         {
-            var nancyHost = new NancyHost(new Uri("http://localhost:8888"), new Uri("http://127.0.0.1:8888"));
+            var nancyHost = new NancyHost(new Bootstrapper.Bootstrapper(), new Uri("http://localhost:8888"), new Uri("http://127.0.0.1:8888"));
             nancyHost.Start();
 
             Console.WriteLine("Nancy now listening - navigating to http://localhost:8888. Press enter to stop");
